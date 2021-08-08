13788d556a9c113cffb01ee88803100f0e27e547
OH WONJU <81530929+PancakeCookie@users.noreply.github.com>
2021-08-08 18:14:50 +0900

    dfs bfs 백준

--- chap5 DFS, BFS/백준문제/baek1260.py
+++ chap5 DFS, BFS/백준문제/baek1260.py
@@ -6,10 +6,8 @@ n, m, v = map(int, input().split())
 graph = []
 for i in range(m):
     a, b = map(int, input().split())
-    list1 = [a, b]
-    list2 = [b, a]
-    graph.append(list1)
-    graph.append(list2)
+    graph.append([a, b])
+    graph.append([b, a])
 # 입력을 뒤집어서 그래프에 추가해서 모든 노드의 연결관계 입력
 
 graph.sort(key=lambda x: x[1])
@@ -24,7 +22,7 @@ def dfs(start):
     print(start, end=" ")
     for i in range(len(graph)):
         if graph[i][0] == start and visited[graph[i][1]] != True:
-            visited[graph[i][1]] = True
+            # visited[graph[i][1]] = True
             dfs(graph[i][1])
 
 
@@ -41,10 +39,10 @@ def bfs(start):
     queue.append(start)  # 큐에 시작 노드 넣고
     visited[start] = True  # 방문처리
     while queue:
-        start = queue.popleft()
-        print(start, end=" ")
+        node = queue.popleft()
+        print(node, end=" ")
         for i in range(len(graph)):
-            if graph[i][0] == start and visited[graph[i][1]] != True:
+            if graph[i][0] == node and visited[graph[i][1]] != True:
                 visited[graph[i][1]] = True
                 queue.append(graph[i][1])
 
@@ -58,3 +56,47 @@ bfs(v)
 # end=" "
 # 이 옵션의 경우 print 문을 이용해 출력을 완료한 뒤의 내용을 수정할 수 있습니다.
 # 기본 값으로는 개행(\n)이 들어가 있으며 이를 사용해 개행을 없애거나 원하는 문자를 입력할 수 있습니다.
+
+# l = [[] for i in range(n + 1)]
+# for i in range(m):
+#     a, b = map(int, input().split())
+#     l[a].append(b)
+#     l[b].append(a)
+#     l[a].sort()
+#     l[b].sort()
+
+# print(l)
+# [[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
+
+# n, m, v = map(int,input().split())
+# graph = [[] for _ in range(n+1)]
+# for i in range(m):
+#     node1 , node2 = map(int,input().split())
+#     graph[node1].append(node2)
+#     graph[node2].append(node1)
+# for i  in range(n+1):
+#     graph[i].sort()
+
+# check_dfs = [False] * (n+1)
+# check_bfs = [False] * (n+1)
+
+# def DFS(v):
+#     check_dfs[v] = True
+#     print(v, end = ' ')
+#     for value in graph[v] :
+#         if check_dfs[value] == False :
+#             DFS(value)
+# DFS(v)
+# print()
+
+# def BFS(v):
+#     check_bfs[v] = True
+#     q = deque([v])
+#     while q:
+#         now = q.popleft()
+#         print(now, end= " ")
+#         for value in graph[now]:
+#             if check_bfs[value] == False :
+#                 q.append(value)
+#                 check_bfs[value] = True
+# BFS(v)
--- 연습장.py
+++ 연습장.py
@@ -1,17 +1,39 @@
-graph = [
-    [],  # 0번 노드 비우기 문제숫자와 인덱스를 맞추기 위해
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7],
-]
-
-graph2 = graph
-
-graph2[1][1] = 9999
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+    node1, node2 = map(int, input().split())
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+for i in range(n + 1):
+    graph[i].sort()
 
 print(graph)
+check_dfs = [False] * (n + 1)
+check_bfs = [False] * (n + 1)
+
+
+def DFS(v):
+    check_dfs[v] = True
+    print(v, end=" ")
+    for value in graph[v]:
+        if check_dfs[value] == False:
+            DFS(value)
+
+
+DFS(v)
+print()
+
+
+def BFS(v):
+    check_bfs[v] = True
+    q = deque([v])
+    while q:
+        now = q.popleft()
+        print(now, end=" ")
+        for value in graph[now]:
+            if check_bfs[value] == False:
+                q.append(value)
+                check_bfs[value] = True
+
+
+BFS(v)
