a43da889752024cf6818411b3da8e340f86c25b3
PancakeCookie <ows969@naver.com>
2021-08-16 09:33:48 +0900

    백준 풀이

---
+++ baek13549.py
@@ -0,0 +1,4 @@
+# 13549 숨바꼭질3
+n, k = map(int, input().split())
+
+print(n, k)
--- chap5 DFS, BFS/백준문제/baek14502.py
+++ chap5 DFS, BFS/백준문제/baek14502.py
@@ -8,10 +8,11 @@ lab = []
 for i in range(n):
     lab.append(list(map(int, input().split())))
 
-lab_copy = copy.deepcopy(lab)
+lab_copy = copy.deepcopy(lab)  # 다차원 리스트의 복사를 위해 import copy, deepcopy 사용
 
-blank = []
-virus = []
+blank = []  # 0의 위치 저장
+virus = []  # 2의 위치 저장
+
 for i in range(n):
     for j in range(m):
         if lab[i][j] == 0:
@@ -19,7 +20,7 @@ for i in range(n):
         if lab[i][j] == 2:
             virus.append([i, j])
 
-result = list(combinations(blank, 3))
+result = list(combinations(blank, 3))  # 3차원 배열
 
 
 def insfection(x, y):
@@ -29,7 +30,7 @@ def insfection(x, y):
     if x + 1 < n and lab_copy[x + 1][y] == 0:
         lab_copy[x + 1][y] = 2
         insfection(x + 1, y)
-    if y - 1 >= 0 and lab_copy[x][y - 1] == 0:
+    if y - 1 >= 0 and lab_copy[x][y - 1] == 0:  # 하니까 x좌표는 체크 안해도됨
         lab_copy[x][y - 1] = 2
         insfection(x, y - 1)
     if y + 1 < m and lab_copy[x][y + 1] == 0:
@@ -37,13 +38,21 @@ def insfection(x, y):
         insfection(x, y + 1)
 
 
+count = 0
+countlist = []
 for i in range(len(result)):
-    a, b, c = map(list, result[i])
-    lab[a[0]][a[1]] = 1
-    lab[b[0]][b[1]] = 1
-    lab[c[0]][c[1]] = 1
+    a, b, c = map(list, result[i])  # 벽 세우기
+    lab_copy[a[0]][a[1]] = 1
+    lab_copy[b[0]][b[1]] = 1
+    lab_copy[c[0]][c[1]] = 1
+    for i in range(len(virus)):  # 감염 시키고
+        insfection(virus[i][0], virus[i][1])
+    for i in range(n):
+        for j in range(m):
+            if lab_copy[i][j] == 0:  # 0 갯수 세고
+                count = count + 1
+    countlist.append(count)  # 카운트에 넣은 다음
+    lab_copy = copy.deepcopy(lab)  # 초기화
+    count = 0
 
-for i in range(len(virus)):
-    insfection(i[0], i[1])
-
-print(lab_copy)
+print(max(countlist))
